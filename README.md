8a482673af1f61e7660b1f49794589954b8339ae
Victor <victorcampos@usp.br>
2020-05-25 18:10:59 -0300

    Add Tetromino shapes to game project.
    Add const arrays consisting of arrays used to draw
    each one of the tetromino shapes, in each respective
    rotation. Color each one of them accordingly by adding
    a .tetromino CSS class.
    Update web page and README.md file accordingly.
    
    Signed-off-by: Victor <victorcampos@usp.br>

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 Simple Tetris game using JavaScript
 
 Basic Tetris game made using JavaScript. This game was made to add some
-interactivity to my personal web page at https://linux.ime.usp.br/~jtrr.
+interactivity to the web page at https://linux.ime.usp.br/~jtrr. Please
+feel free to use as you wish!
--- app.js
+++ app.js
@@ -1,5 +1,61 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid') // const grid receives the .grid values from html file
+    let squares = Array.from(document.querySelectorAll('.grid div')) // Tuns divs into array with specific index number
+    const width = 10
+    const scoreDisplay = document.querySelector('#score') // span id = score
+    const startButton = document.querySelector('#start-button') 
 
+    // Tetraminós
+
+    const lTetromino = [ // Draw each of the 5 tetrominoes in each of its 4 rotations using our width const
+        [1, width+1, (width*2)+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, (width*2)+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+    ]
+
+    const iTetromino = [
+        [1, width+1, (width*2)+1, (width*3)+1],
+        [width,width+1,width+2,width+3],
+        [1, width+1, (width*2)+1, (width*3)+1],
+        [width,width+1,width+2,width+3]
+    ]
+
+    const tTetromino = [
+        [width, width+1, width+2, (width*2)+1],
+        [width+1, width*2, (width*2)+1, (width*2)+2],
+        [width+1, width*2, (width*2)+1, (width*3)+1],
+        [width, width*2, width*3, (width*2)+1]
+    ]
+
+    const oTetromino = [
+        [width, width+1, width*2, (width*2)+1],
+        [width, width+1, width*2, (width*2)+1],
+        [width, width+1, width*2, (width*2)+1],
+        [width, width+1, width*2, (width*2)+1]
+    ]
+
+    const zTetromino = [
+        [width, width*2, (width*2)+1, (width*3)+1],
+        [width+1, width+2, width*2, (width*2)+1],
+        [width, width*2, (width*2)+1, (width*3)+1],
+        [width+1, width+2, width*2, (width*2)+1]
+    ]
+
+    const tetrominoes = [lTetromino, iTetromino, tTetromino, oTetromino, zTetromino]
+
+    let currentPosition = 4
+    let current = tetrominoes[0][0]
+
+   // Draw first rotation in the first tetramino
+
+   function draw() {
+       current.forEach(index => {
+           squares[currentPosition + index].classList.add('tetromino') // Add class 'tetromino' for each tetramino position in the array, as to color those.
+       })
+   }
+
+   draw()
 
 
 
